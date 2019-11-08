385970bc22476d404155ce3123a2882ea4f6b8c4
MohammadReza Vafaei <imohammad.vafaei@gmail.com>
2019-11-08 16:01:35 +0330

    add toast component

---
+++ src/components/Toast.vue
@@ -0,0 +1,34 @@
+<template>
+  <div :class="['toaster', `toaster--${type}`]" v-if="!isHide">
+    <slot></slot>
+    <span class="glyphicon glyphicon-remove" @click="isHide = !isHide"></span>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    type: String,
+    isHide: Boolean
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+.toaster {
+  position: fixed;
+  top: 20px;
+  right: 20px;
+  padding: 10px 20px;
+
+  &--success {
+
+  }
+
+  &--error {
+    background-color: #efdfdf;
+    color: #d9534f;
+  }
+
+}
+</style>
