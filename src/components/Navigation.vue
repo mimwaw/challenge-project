b3495fca340e1930f3d1e728b3c95a3de660456f
MohammadReza Vafaei <imohammad.vafaei@gmail.com>
2019-11-08 15:20:47 +0330

    add top navigation

---
+++ src/components/Navigation.vue
@@ -0,0 +1,45 @@
+<template>
+  <div class="nav">
+    <div class="container-fluid">
+      <div class="row align-items-center">
+        <div class="col-lg-2">
+          <h4>Arvan Challenge</h4>
+        </div>
+        <div class="col-lg-2">
+          <span>Welcome, {{ username }}</span>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import api from "../api/api";
+
+export default {
+  data() {
+    return {
+      username: null
+    };
+  },
+  created() {
+    this.fetchUsername();
+  },
+  methods: {
+    fetchUsername() {
+      api.get("/user").then((res) => {
+        this.username = res.data.user.username;
+      });
+    }
+  }
+};
+</script>
+
+<style lang="scss" scoped>
+.nav {
+  background-color: #373a3c;
+  height: 60px;
+  color: white;
+  padding: 10px 0;
+}
+</style>
