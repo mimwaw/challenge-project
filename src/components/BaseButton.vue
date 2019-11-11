631cf9c33bd31de81e669c15f94a7a3930d1ed0d
MohammadReza Vafaei <imohammad.vafaei@gmail.com>
2019-11-11 13:21:31 +0330

    add essential components

---
+++ src/components/BaseButton.vue
@@ -0,0 +1,22 @@
+<template>
+  <button :type="btnType" :class="btnClass" :disabled="isLoading">
+    <slot v-if="!isLoading"></slot>
+    <span v-else class="spinner-border spinner-border-sm" role="status" aria-hidden="true"></span>
+  </button>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      
+    }
+  },
+  props : {
+    btnType: String,
+    btnClass: String,
+    isLoading: Boolean
+  }
+
+}
+</script>
