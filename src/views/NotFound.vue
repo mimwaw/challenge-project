072ae4d6e0a8395dd9677b7ee3e7153f6f6ac35e
MohammadReza Vafaei <imohammad.vafaei@gmail.com>
2019-11-13 17:10:39 +0330

    add 404 NotFound page

---
+++ src/views/NotFound.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="wrapper d-flex align-items-center">
+    <div class="container">
+      <div class="row align-items-center justify-content-center">
+        <p>Error 404, Not Found!</p>
+      </div>
+      <div class="row align-items-center justify-content-center">
+        <button class="btn btn-outline-info" @click="$router.go(-2)">
+          Go Back
+        </button>
+      </div>
+    </div>
+  </div>
+</template>
+
+<style lang="scss" scoped>
+.wrapper {
+  height: 100vh;
+}
+</style>
